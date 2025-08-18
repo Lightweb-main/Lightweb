ce213a54ba88bd9841eb06672faf7a46a67165bd
Idkwhyididthissome <idkwhyididthissome2@gmail.com>
2025-08-17 18:32:12 -0700

    Update UnitConverter.js

--- Utility/UnitConverter.js
+++ Utility/UnitConverter.js
@@ -129,12 +129,13 @@ switch (to) {
     case "celsius": convertedValue2 = celsius; break;
     case "fahrenheit": convertedValue2 = (celsius * 9/5) + 32; break;
     case "kelvin": convertedValue2 = celsius + 273.15; break;
-    case "rankine": convertedValue2 = (value * 1.8) + 491.67; break;
+    case "rankine": convertedValue2 = (celsius + 273.15) * 9/5; break;
     default: convertedValue2 = NaN;
   }
     document.getElementById("result2").value = convertedValue2.toFixed(decimalPlaces2);
     document.getElementById("resultSciNot2").value = convertedValue2.toExponential(2);
 }
+
 
 
 
