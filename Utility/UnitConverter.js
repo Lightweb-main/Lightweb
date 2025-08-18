81577c73cfd1f7b9bb6c5a9185b47e87146bc59c
Idkwhyididthissome <idkwhyididthissome2@gmail.com>
2025-08-17 20:18:40 -0700

    Update UnitConverter.js

--- Utility/UnitConverter.js
+++ Utility/UnitConverter.js
@@ -18,8 +18,6 @@ document.getElementById("inputValue2").value = 100;
 document.getElementById("decimalAmount1").value = "2"
 document.getElementById("decimalAmount2").value = "2"
 
-convert();
-convert2();
 const conversionRates1 = {
     quectometer: 10**30,
     rontometer: 10**27,
@@ -149,5 +147,6 @@ switch (to) {
     document.getElementById("result2").value = convertedValue2.toFixed(decimalPlaces2);
     document.getElementById("resultSciNot2").value = convertedValue2.toExponential(2);
 }
+
 
 
