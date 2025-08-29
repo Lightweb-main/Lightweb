63e6dc128ab3a3e7c7c9b44460497d5c4a159c63
Idkwhyididthissome <idkwhyididthissome2@gmail.com>
2025-08-28 20:25:48 -0700

    Update DigitalClock.js

--- CSS/DigitalClock.js
+++ CSS/DigitalClock.js
@@ -141,6 +141,13 @@ iconthingy.href = '../Images/Lightweb.icon.png?v=' + faviconNum
 
 document.head.appendChild(iconthingy);
 
+document.addEventListener("contextmenu", e => e.preventDefault());
+document.addEventListener("keydown", e => {
+  if (e.key === "F12" || (e.ctrlKey && e.shiftKey && e.key === "I")) {
+    e.preventDefault();
+  }
+});
+
 
 
 
