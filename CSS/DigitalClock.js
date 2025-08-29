ccd856db0890dfe5b4b745a538b1d51bbc862aa7
Idkwhyididthissome <idkwhyididthissome2@gmail.com>
2025-08-28 20:39:25 -0700

    Update DigitalClock.js

--- CSS/DigitalClock.js
+++ CSS/DigitalClock.js
@@ -145,8 +145,12 @@ document.addEventListener("contextmenu", e => e.preventDefault());
 document.addEventListener("keydown", e => {
   if (e.key === "F12" || (e.ctrlKey && e.shiftKey && e.key === "I")) {
     e.preventDefault();
+	window.location.href = "about:blank";
   }
 });
+
+
+
 
 
 
