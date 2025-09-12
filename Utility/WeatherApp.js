4b36e3a804c6f8d6371a2613cb9a6ddc0cfc7bfb
Idkwhyididthissome <idkwhyididthissome2@gmail.com>
2025-09-12 08:24:24 -0700

    Create WeatherApp.js

---
+++ Utility/WeatherApp.js
@@ -0,0 +1,4 @@
+const url = 'https://api.openweathermap.org/data/2.5/weather'
+const apiKey = '6724f2d5e07375722cfcb71ecd81db7e'
+let location = "Los%20Angeles"
+const url = `${apiUrl}?q=${location}&appid=${apiKey}&units=metric`;
