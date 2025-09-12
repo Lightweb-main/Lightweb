4e871594c6751b5bd1c5da3399b14aa39a5ac78b
Idkwhyididthissome <idkwhyididthissome2@gmail.com>
2025-09-12 13:18:38 -0700

    Update WeatherApp.js

--- Utility/WeatherApp.js
+++ Utility/WeatherApp.js
@@ -2,7 +2,7 @@ const temperatureDisplay = document.getElementById("temperatureDisplay");
 
 const url = 'https://api.openweathermap.org/data/2.5/weather'
 const apiKey = '6724f2d5e07375722cfcb71ecd81db7e'
-let location = "Los%20Angeles"
+let location = "Los%20Angeles" 
 const url = `${apiUrl}?q=${location}&appid=${apiKey}&units=metric`;
 
 
