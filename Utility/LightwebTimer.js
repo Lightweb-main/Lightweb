c420b7218627994dc515e49fafdf03ed8c663827
Idkwhyididthissome <idkwhyididthissome2@gmail.com>
2025-09-21 20:43:03 -0700

    Add files via upload

--- Utility/LightwebTimer.js
+++ Utility/LightwebTimer.js
@@ -1,8 +1,8 @@
 let timerInterval;
 const timer = document.getElementById('DigitalTimer');
-let seconds = 0;
-let minutes = 0;
-let hours = 0;
+let seconds = parseInt(localStorage.getItem("LightwebTimer-Seconds")) || 0;
+let minutes = parseInt(localStorage.getItem("LightwebTimer-Minutes")) || 0;
+let hours = parseInt(localStorage.getItem("LightwebTimer-Hours")) || 0;
 let timerToggle = false;
 
 function updateDisplay() {
@@ -23,14 +23,17 @@ function countdown() {
 
   if (seconds > 0) {
     seconds--;
+	  saveTime();
   } else {
     if (minutes > 0) {
       minutes--;
       seconds = 59;
+	  saveTime();
     } else if (hours > 0) {
       hours--;
       minutes = 59;
       seconds = 59;
+	  saveTime();
     }
   }
 	
@@ -49,77 +52,96 @@ function stop() {
   clearInterval(timerInterval);
 }
 
+function saveTime(){
+localStorage.setItem("LightwebTimer-Seconds", seconds);
+  localStorage.setItem("LightwebTimer-Minutes", minutes);
+  localStorage.setItem("LightwebTimer-Hours", hours);	
+}
 function onesecondAdd() {
   seconds += 1;
   normalizeTime();
   updateDisplay();
+  saveTime();
 }
 
 function fivesecondAdd() {
   seconds += 5;
   normalizeTime();
   updateDisplay();
+  saveTime();
 }
 
 function tensecondAdd() {
   seconds += 10;
   normalizeTime();
   updateDisplay();
+  saveTime();
 }
 function thirtysecondAdd() {
   seconds += 30;
   normalizeTime();
   updateDisplay();
+  saveTime();
 }
 function oneMinuteAdd() {
   minutes += 1;
   normalizeTime();
   updateDisplay();
+  saveTime();
 }
 function twoMinuteAdd() {
   minutes += 2;
   normalizeTime();
   updateDisplay();
+  saveTime();
 }
 function fiveMinuteAdd() {
   minutes += 5;
   normalizeTime();
   updateDisplay();
+  saveTime();
 }
 function tenMinuteAdd() {
   minutes += 10;
   normalizeTime();
   updateDisplay();
+  saveTime();
 }
 function thirtyMinuteAdd() {
   minutes += 30;
   normalizeTime();
   updateDisplay();
+  saveTime();
 }
 function oneHourAdd() {
   hours += 1;
   normalizeTime();
   updateDisplay();
+  saveTime();
 }
 function twoHourAdd() {
   hours += 2;
   normalizeTime();
   updateDisplay();
+  saveTime();
 }
 function sixHourAdd() {
   hours += 6;
   normalizeTime();
   updateDisplay();
+  saveTime();
 }
 function twelveHourAdd() {
   hours += 12;
   normalizeTime();
   updateDisplay();
+  saveTime();
 }
 function twentyfourHourAdd() {
   hours += 24;
   normalizeTime();
   updateDisplay();
+  saveTime();
 }
 
 function resetTimer(){
@@ -127,6 +149,7 @@ seconds = 0;
 minutes = 0;
 hours = 0;
 normalizeTime();
+saveTime();
   updateDisplay();
 }
 function normalizeTime() {
@@ -140,3 +163,4 @@ function normalizeTime() {
   }
 
 }
+updateDisplay();
