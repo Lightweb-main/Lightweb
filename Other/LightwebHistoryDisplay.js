3a4eed0c6ed9ad2937be14e1df4e8788c9b7057a
Idkwhyididthissome <idkwhyididthissome2@gmail.com>
2025-11-25 21:10:26 -0800

    Delete Other directory

--- Other/LightwebHistoryDisplay.js
+++
@@ -1,9 +0,0 @@
-fetch('../Other/LightwebHistory.html')
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById('fileContent').textContent = data;
-  })
-  .catch(err => {
-    document.getElementById('fileContent').textContent = 'Error loading file.';
-    console.error(err);
-  });
